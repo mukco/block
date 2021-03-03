8ae5544529726c9bce8088633a6b51a027eed465
mukco <THREExPENNYco@gmail.com>
2021-03-03 00:27:30 -0700

    Blockchain

---
+++ Block.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+class Block { 
+public:
+	string sPrevHash;
+	Block(uint32_t nIndexIn, const string &sDataIn);
+	string GetHash();
+	void MineBlock(uint32_t nDifficulty);
+
+private:
+	uint32_t _nIndex;
+	int64_t _nNonce;
+	string _sData;
+	string _sHash;
+	time_t _tTime;
+
+	string _CalculateHash() const; // const member functions will not modify any member data
+};
